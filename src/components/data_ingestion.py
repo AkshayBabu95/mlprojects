549695ec708b38082f2073c32c296526038c3e77
akshaybabu <akshaybabu2019@gmail.com>
2025-03-11 00:06:54 -0400

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTranformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -22,7 +24,9 @@ class DataIngestion:
         try:
             df = pd.read_csv('notebook\data\stud.csv')
             logging.info("Read dataset as dataframe")
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path))
+            # os.makedirs(os.path.dirname(self.ingestion_config.train_data_path))
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             logging.info('Train test split initated')
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
@@ -41,4 +45,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
